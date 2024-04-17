85b0e1d2d9993abeaa9a06d6ea2b7ef5f1b02e69
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2024-04-17 17:03:04 +0200

    ok

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
-st.title('Projet 4 - AgriBalyse')
+st.title('Projet 4A - AgriBalyse')
 
 #ajouter une image
 st.image('Agribalyse-image1.png')
@@ -36,12 +36,17 @@ st.header('Aficher nos datasets traités (ou bruts)')
 
 ### du code qui vient de notre notebook, ne fait pas partie de l'affichage
 data = pd.read_csv('Agribalyse_Synthese (2).csv')
+cols = list(data.columns)
+data = data.rename(columns={cols[18]: "effets_toxico_non_cancer", cols[19]: "effets_toxico_cancer"})
 
 ## ici oui on l'affiche avec st
 st.write("Les données AgriBalyse brutes")
 st.dataframe(data)
 
 st.divider() 
+
+data.describe()
+
 st.write("Des buttons")
 
 st.button("Groupes d'aliments")
