1df7f04cbfbbd3aefdc2e493d1e0a23b97be9c12
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2024-05-03 08:39:38 +0000

    ok

---
+++ app_agribalyse.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.page_link("app_agribalyse.py", label="Home", icon="🏠")
+st.page_link("pages/application.py", label="Page 1", icon="1️⃣")
+st.page_link("http://www.google.com", label="Google", icon="🌎")
