19ea8cca0488696365830727cb0b6f2bd657cc96
hamiltonrmat <johndoe@example.com>
2024-05-03 11:48:33 +0200

    ok

--- Homepage.py
+++ Homepage.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.title('Projet 4A - Traitement de données alimentaires')
+
 st.page_link("app_agribalyse.py", label="Homepage", icon="🏠")
 st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
 st.page_link("http://www.google.com", label="Google", icon="🌎")
