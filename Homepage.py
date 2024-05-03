58a9c6418c7b0a1eeab5ef5cbfe99c3020c368af
hamiltonrmat <johndoe@example.com>
2024-05-03 14:21:27 +0200

    ok

--- Homepage.py
+++ Homepage.py
@@ -2,8 +2,15 @@ import streamlit as st
 
 st.title('Projet 4A - Traitement de données alimentaires')
 
-st.page_link("Homepage.py", label="Homepage", icon="🏠")
-st.page_link("pages/Introduction.py", label="Introduction", icon="1️⃣")
-st.page_link("pages/Vision_génerale.py", label="Vision génerale", icon="2️⃣")
-st.page_link("pages/Score_unique_EF.py", label="Score_unique_EF", icon="2️⃣")
-st.page_link("https://agribalyse.ademe.fr/", label="Page offitielle Ademe", icon="🌎")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.page_link("Homepage.py", label="Homepage", icon="🏠")
+    st.page_link("pages/Introduction.py", label="Introduction", icon="1️⃣")
+    st.page_link("pages/Vision_génerale.py", label="Vision génerale", icon="2️⃣")
+    st.page_link("pages/Score_unique_EF.py", label="Score_unique_EF", icon="2️⃣")
+    st.page_link("https://agribalyse.ademe.fr/", label="Page offitielle Ademe", icon="🌎")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
