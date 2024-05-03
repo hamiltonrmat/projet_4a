87edf02b161290022be7344eac8485ccfadd6df5
hamiltonrmat <johndoe@example.com>
2024-05-03 11:57:40 +0200

    ok

--- Homepage.py
+++ Homepage.py
@@ -4,5 +4,5 @@ st.title('Projet 4A - Traitement de données alimentaires')
 
 st.page_link("Homepage.py", label="Homepage", icon="🏠")
 st.page_link("pages/page_1.py", label="Agribalyse c'est quoi ?", icon="1️⃣")
-st.page_link("pages/page_1.py", label="Introduction à la base de données", icon="1️⃣")
+st.page_link("pages/page_2.py", label="Introduction à la base de données", icon="1️⃣")
 st.page_link("https://agribalyse.ademe.fr/", label="Page offitielle Ademe", icon="🌎")
---
+++ pages/page_2.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import os,sys,inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir) 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+import plotly.express as px
+
+st.title('Projet 4A - Traitement de données alimentaires')
