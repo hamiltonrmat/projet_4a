6edea13421fea891f213700d5c442a6089badc6b
hamiltonrmat <johndoe@example.com>
2024-05-03 17:52:09 +0200

    ok

--- pages/2_📊_Vision_génerale.py
+++ pages/2_📊_Vision_génerale.py
@@ -62,8 +62,10 @@ confirm = st.checkbox('Afficher détails')
 
 if confirm:
     st.write(variable_select)
-    fig = px.box(df[variable_select], y=variable_select)
+    fig = px.histogram(df[variable_select], x=variable_select, nbins=10)
     st.plotly_chart(fig, use_container_width=True)
+    fig2 = px.box(df[variable_select], y=variable_select)
+    st.plotly_chart(fig2, use_container_width=True)
     min_variable, max_variable, mean_variable = st.columns(3)
     min_variable.metric("Min", np.round(df[variable_select].min(), 3))
     max_variable.metric("Max", np.round(df[variable_select].max(), 3))
