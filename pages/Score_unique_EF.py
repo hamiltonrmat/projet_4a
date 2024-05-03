11a509856d5c58cac1fc8877d331e90790d89397
hamiltonrmat <johndoe@example.com>
2024-05-03 14:14:02 +0200

    ok

--- pages/Score_unique_EF.py
+++ pages/Score_unique_EF.py
@@ -12,6 +12,10 @@ import plotly.express as px
 data = pd.read_csv('Agribalyse_Synthese (2).csv')
 cols = list(data.columns)
 data = data.rename(columns={cols[18]: "effets_toxico_non_cancer", cols[19]: "effets_toxico_cancer"})
+st.header('Qualité de la donnée:')
+dqr_value = st.select_slider('Qualité de la donné',
+    options=[1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5])
+st.write('Donnés avec DQR plus petit que:', dqr_value)
 df = data[data["DQR"]<dqr_value]
 
 st.header('Le score unique EF')
