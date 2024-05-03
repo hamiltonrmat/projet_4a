102c220e8e3444820367a5e52d1f409aeecb9f6f
hamiltonrmat <johndoe@example.com>
2024-05-03 11:11:25 +0200

    ok

--- app_agribalyse.py
+++ app_agribalyse.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.page_link("app_agribalyse.py", label="Home", icon="🏠")
-st.page_link("application.py", label="Page 1", icon="1️⃣")
+st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
 st.page_link("http://www.google.com", label="Google", icon="🌎")
