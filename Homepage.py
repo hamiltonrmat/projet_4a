f7da5c906034010ef4c1d046ead057f512cf285d
hamiltonrmat <johndoe@example.com>
2024-05-03 16:21:19 +0200

    ok

--- Homepage.py
+++ Homepage.py
@@ -10,7 +10,7 @@ with col1:
     st.page_link("Homepage.py", label="Homepage", icon="🏠")
     st.page_link("pages/1_📚_Introduction.py", label="Introduction", icon="1️⃣")
     st.page_link("pages/2_📊_Vision_génerale.py", label="Vision génerale", icon="2️⃣")
-    st.page_link("pages/3_📈_Score_unique_EF.py", label="Score_unique_EF", icon="2️⃣")
+    st.page_link("pages/3_📈_Score_unique_EF.py", label="Score_unique_EF", icon="3️⃣")
     st.page_link("https://agribalyse.ademe.fr/", label="Page offitielle Ademe", icon="🌎")
 
 with col2:
