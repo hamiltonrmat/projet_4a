4414d740c122d03a1a97bd1b7de319eb48c25b44
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2024-05-22 18:18:51 +0200

    Update Comparatif.py

--- pages/Comparatif.py
+++ pages/Comparatif.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import numpy as np
+import os,sys,inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir) 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+import plotly.express as px
+
+st.title("Comparatif")
+
 
