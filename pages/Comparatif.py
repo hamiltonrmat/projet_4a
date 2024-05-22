c4b1f54c11ded667ad7a7a3e0f42a2bd8624cb2b
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2024-05-22 18:52:36 +0200

    Update Comparatif.py

--- pages/Comparatif.py
+++ pages/Comparatif.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 import plotly.express as px
-from scikit-learn.preprocessing import MinMaxScaler
+from scikit.preprocessing import MinMaxScaler
 
 
 st.title("Comparatif")
