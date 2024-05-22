105f68bd103cae4632ed433dc0a91e439a5aede4
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2024-05-22 13:30:14 +0200

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -4,11 +4,9 @@ import streamlit as st
 st.set_page_config(page_title="AgriBalyse UniLaSalle", initial_sidebar_state='auto', layout="wide")
 st.title('Projet 4A - Traitement de données alimentaires')
 
-col1, col2 = st.columns(2)
-
 st.image("https://www.static.inrae.fr/cdn/ff/VoSKX7-O_CnYVPGPXTYLwmevKPz0YGDsQwPriPEhhNI/1601368205/public/png/bandeau%20Agribalyse.png")
 
-
+col1, col2 = st.columns(2)
 with col1:
     st.page_link("Homepage.py", label="Homepage", icon="🏠")
     st.page_link("pages/1_📚_Introduction.py", label="Introduction", icon="1️⃣")
