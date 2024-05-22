abefe8488214fa1d152f0a3910b7306ef5ced885
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2024-05-22 15:20:00 +0200

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -4,15 +4,14 @@ import streamlit as st
 st.set_page_config(page_title="AgriBalyse UniLaSalle", initial_sidebar_state='auto', layout="wide")
 st.title('Projet 4A - Traitement de données alimentaires')
 
-st.image("https://www.actalia.eu/wp-content/uploads/2020/07/Agribalyse.jpg")
+st.page_link("Homepage.py", label="Homepage", icon="🏠")
+st.page_link("pages/1_📚_Introduction.py", label="Introduction", icon="1️⃣")
+st.page_link("pages/2_📊_Vision_génerale.py", label="Vision génerale", icon="2️⃣")
+st.page_link("pages/3_📈_Score_unique_EF.py", label="Score_unique_EF", icon="3️⃣")
+st.page_link("https://agribalyse.ademe.fr/", label="Page offitielle Ademe", icon="🌎")
 
 col1, col2 = st.columns(2)
 with col1:
-    st.page_link("Homepage.py", label="Homepage", icon="🏠")
-    st.page_link("pages/1_📚_Introduction.py", label="Introduction", icon="1️⃣")
-    st.page_link("pages/2_📊_Vision_génerale.py", label="Vision génerale", icon="2️⃣")
-    st.page_link("pages/3_📈_Score_unique_EF.py", label="Score_unique_EF", icon="3️⃣")
-    st.page_link("https://agribalyse.ademe.fr/", label="Page offitielle Ademe", icon="🌎")
-
+    st.image("https://www.actalia.eu/wp-content/uploads/2020/07/Agribalyse.jpg")
 with col2:
    st.image("https://www.actalia.eu/wp-content/uploads/2020/07/Agribalyse.jpg")
